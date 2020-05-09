64d3bf94ff1d5878fc2c6888c6e0132aae0ede1e
mariuscretu <valentin.cretu@info.uaic.ro>
2020-05-09 21:45:04 +0300

    Added compulsory

---
+++ Lab_10/ServerApplication/src/com/company/ClientThread.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+enum Command {
+    stop
+}
+
+public class ClientThread extends Thread {
+    private Socket socket = null;
+    private boolean closeServer = false;
+
+    public ClientThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            System.err.println("Starting thread execution ...");
+            // Get the request from the input stream: client → server
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            String request = in.readLine();
+            // Send the response to the oputput stream: server → client
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            String command = handleClientCommand(request);
+            out.println(command);
+            out.flush();
+        } catch (IOException e) {
+            System.err.println("Communication error... " + e);
+        } finally {
+            try {
+                socket.close(); // or use try-with-resources
+                System.err.println("The socket created for client communication has been closed!");
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    public boolean getThreadResponse() {
+        return closeServer;
+    }
+
+    private String handleClientCommand(String command) {
+        if (command.compareTo(String.valueOf(Command.stop)) == 0) {
+            closeServer = true;
+            return "Server stopped";
+        }
+        return "Server received the request ...";
+    }
+}
